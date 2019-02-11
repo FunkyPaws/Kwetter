8a5d1327ca280c45b3c580d569664fb7078332d1
Genise <springtouw@hotmail.com>
2019-02-11 19:26:42 +0100

    Initial Commit add - user.java - field/constructor/getset - post.java - field/constructor/getset
    changed
    pom.xml add dependencies

---
+++ src/main/Java/Domain/Post.java
@@ -0,0 +1,83 @@
+package domain;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+public class Post {
+    //Properties
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long PostID;
+
+    private String Text;
+    private Date DateTime;
+    private Boolean IsReaction;
+    private User User;
+
+    private List<Post> Reactions;
+
+    // Constructors
+    public Post(String text, Date dateTime, Boolean isReaction, domain.User user, List<Post> reactions) {
+        Text = text;
+        DateTime = dateTime;
+        IsReaction = isReaction;
+        User = user;
+        Reactions = reactions;
+    }
+
+    // Getters and Setters
+    public Long getPostID() {
+        return PostID;
+    }
+
+    public void setPostID(Long postID) {
+        PostID = postID;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+
+    public Date getDateTime() {
+        return DateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        DateTime = dateTime;
+    }
+
+    public Boolean getReaction() {
+        return IsReaction;
+    }
+
+    public void setReaction(Boolean reaction) {
+        IsReaction = reaction;
+    }
+
+    public domain.User getUser() {
+        return User;
+    }
+
+    public void setUser(domain.User user) {
+        User = user;
+    }
+
+    public List<Post> getReactions() {
+        return Reactions;
+    }
+
+    public void setReactions(List<Post> reactions) {
+        Reactions = reactions;
+    }
+
+    // Methods
+
+}
--- src/main/Java/Domain/User.java
+++ src/main/Java/Domain/User.java
@@ -1,4 +1,110 @@
-package Domain;
+package domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
 
 public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long UserID;
+
+    private String Name;
+    private String Password;
+    private String Biography;
+    private String Location;
+    private String Website;
+    private Boolean IsAdmin;
+
+    private List<User> Followers;
+    private List<User> Following;
+
+    // Constructors
+    public User(String name, String password, String biography, String location, String website, Boolean isAdmin, List<User> followers, List<User> following) {
+        Name = name;
+        Password = password;
+        Biography = biography;
+        Location = location;
+        Website = website;
+        IsAdmin = isAdmin;
+        Followers = followers;
+        Following = following;
+    }
+
+    // Getters and Setters
+    public Long getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(Long userID) {
+        UserID = userID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getBiography() {
+        return Biography;
+    }
+
+    public void setBiography(String biography) {
+        Biography = biography;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getWebsite() {
+        return Website;
+    }
+
+    public void setWebsite(String website) {
+        Website = website;
+    }
+
+    public Boolean getAdmin() {
+        return IsAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        IsAdmin = admin;
+    }
+
+    public List<User> getFollowers() {
+        return Followers;
+    }
+
+    public void setFollowers(List<User> followers) {
+        Followers = followers;
+    }
+
+    public List<User> getFollowing() {
+        return Following;
+    }
+
+    public void setFollowing(List<User> following) {
+        Following = following;
+    }
+
+    // Methods
+
 }
